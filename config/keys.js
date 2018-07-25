753e772a944d8a9b8b47e81f8428eae9eac34d73
Shahrear002 <shabuz2900@gmail.com>
2018-07-26 03:02:27 +0600

    Upto ProfileSchema

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    'mongodb://shahrear002:shabuz2222@ds253871.mlab.com:53871/firstmernapp'
+    'mongodb://shahrear002:shabuz2222@ds253871.mlab.com:53871/firstmernapp',
+  secretOrKey: 'secret'
 };
